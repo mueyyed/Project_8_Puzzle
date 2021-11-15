2f20658066d13444cf87aa7af27cd28931782736
mueyyed <68438639+mueyyed@users.noreply.github.com>
2021-11-15 22:59:27 +0300

    Delete trial.py

--- trial.py
+++
@@ -1,8 +0,0 @@
-
-samples="                1223456789              "
-samples2 = "                 bound                 "
-
-samples = samples.strip()
-samples2= samples2.strip()
-
-print(samples2 + samples )
